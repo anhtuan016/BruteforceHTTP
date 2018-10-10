6e53108aef8756c8c6021b995e0940af1dd86219
Ngoc Bui Dinh <buidinhngoc.aiti@gmail.com>
2018-10-10 18:21:41 +0700

    Fix 'No module named core' when install script

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,9 @@
-pass
+# -*- coding: utf-8 -*-
+__title__ = 'BruteforceHTTP Core module'
+__version__ = '0.1'
+__author__ = 'ngocbd'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017-2018 Bui Dinh Ngoc'
+"""
+Core module BruteforceHTTP include tbrowser,actions,options
+"""
